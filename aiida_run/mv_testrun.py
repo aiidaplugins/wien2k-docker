94105076d83dcf50ab9a41b8a5b40ff899c62112
Marnik Bercx <mbercx@gmail.com>
2023-09-03 09:44:03 +0200

    👌 Clean up duplicate `aiida_run` folder

--- aiida_run/mv_testrun.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-cj_path = Path(QueryBuilder().append(CalcJobNode).first()[0].get_remote_workdir())
-
-case_path = cj_path / 'case'
-case_path.rename('/home/aiida/aiida_run/case')
-
-stderr_path = cj_path / '_scheduler-stderr.txt'
-stderr_path.rename('/home/aiida/aiida_run/case/stderr.log')
--- aiida_run/submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo '🚀 Running WIEN2k AiiDA test'
-aiida-common-workflows launch relax -r none -S Si -p moderate wien2k
-verdi run /home/aiida/aiida_run/mv_testrun.py
